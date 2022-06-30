647426a2d4d4526c6e956c033f60340a1c734049
dcamposm <m8.demetrio.campos@gmail.com>
2022-06-30 21:11:21 +0200

    App directory

---
+++ app/src/Entity/Task.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $id_user;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'datetime')]
+    private $time_start;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $time_end;
+	
+	public function __construct($id_user, $description, $time_start, $time_end = null) {
+		$this->id_user = $id_user;
+		$this->description = $description;
+		$this->time_start = $time_start;
+		$this->time_end = $time_end;
+	}
+	
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(int $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTimeStart(): ?\DateTimeInterface
+    {
+        return $this->time_start;
+    }
+
+    public function setTimeStart(\DateTimeInterface $time_start): self
+    {
+        $this->time_start = $time_start;
+
+        return $this;
+    }
+
+    public function getTimeEnd(): ?\DateTimeInterface
+    {
+        return $this->time_end;
+    }
+
+    public function setTimeEnd(?\DateTimeInterface $time_end): self
+    {
+        $this->time_end = $time_end;
+
+        return $this;
+    }
+}
---
+++ app/src/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $name;
+	
+	public function __construct($name) {
+		$this->name = $name;
+	}
+	
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
